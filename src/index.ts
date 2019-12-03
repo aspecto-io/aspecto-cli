e02f94b82d41a2ad919d8ed86caff9cdd667e64b
Michael Haberman <michael@aspecto.io>
2019-12-03 16:56:44 +0200

    Demo cli

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import * as commander from "commander";
+import handleTestAction from "./handlers/tests.handler";
+const program = new commander.Command();
+program.version('0.0.1');
+
+program
+  .command('run')
+  .description('Run API Tests')
+  .option('--allow-fail',"Whether to fail the process",false)
+  .option('--allow-methods', "Which type of http request to test", "get,post,put,delete")
+  .option('--mono-repo',"Whether it is a mono repo",false)
+  .option('--service-name [service]',"Which service to test; default is the current service by package.json")
+  .option('--git-hash [git]',"Force test for specific git hash; by default will use the current code")
+  // .requiredOption('-t --token <token>',"Your authentication token")
+  .action(handleTestAction)
+
+  
+
+program.parse(process.argv);
+
---
+++ src/services/apiRequest.ts
@@ -0,0 +1,9 @@
+import * as request from "request-promise-native";
+
+export async function sendApiRequest(i){
+    return await request.get(`http://localhost:3322/item/${i}`, {
+        headers:{
+            "x-key":"123"
+        }
+    });
+}
