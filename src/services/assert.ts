1c4ce19531f2ca7ff505e6df8bea391f9a284578
Nir Hadassi <nir@Nirs-MacBook-Pro-2.local>
2020-01-16 17:28:28 +0200

    Added hostname

--- src/services/assert.ts
+++ src/services/assert.ts
@@ -1,10 +1,12 @@
 import { client } from './api-tests-client';
 import { AssertionResponse } from '../types';
+import * as os from 'os';
 
 export const assert = async (payload: any, fetchTestsDuration: number, runTestsDuration: number) => {
     const res = await client.post<AssertionResponse[]>('/assert', {
         options: global.aspectoOptions,
         url: global.url,
+        hostname: os.hostname(),
         fetchTestsDuration,
         runTestsDuration,
         runResult: payload,
