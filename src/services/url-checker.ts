6cd893aee87a31ccdd18eb5533156e5c4ccae7f0
Nir Hadassi <nir@Nirs-MacBook-Pro-2.local>
2020-01-21 16:31:40 -0800

    Fix url checker

--- src/services/url-checker.ts
+++ src/services/url-checker.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 export default async (url: string) => {
     try {
-        await axios.head(url);
+        await axios.head(url, {
+            validateStatus: () => true,
+        });
         return true;
     } catch (err) {
         return false;
