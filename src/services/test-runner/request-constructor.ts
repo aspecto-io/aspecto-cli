8078fa11df5e5de05c6f7b4d260c17ad1b6352b4
Nir Hadassi <nir@Nirs-MacBook-Pro-2.local>
2020-04-01 16:05:51 +0300

    file split

--- src/services/test-runner/request-constructor.ts
+++ src/services/test-runner/request-constructor.ts
@@ -1,5 +1,6 @@
 import { AspectoTest, RuleTypes, AssignmentRule, AssignOn } from '../../models';
 import { AxiosRequestConfig, Method } from 'axios';
+import constructHeaders from './headers-constructor';
 import getDynamicParam from './get-dynamic-parameter';
 
 const constructQuery = (queryObject: Record<string, string> = {}, assignmentRules: AssignmentRule[]): string => {
@@ -15,46 +16,6 @@ const constructQuery = (queryObject: Record<string, string> = {}, assignmentRule
         } else query.push(`${queryName}=${encodeURIComponent(value)}`);
     });
     return query.length > 0 ? '?' + query.join('&') : '';
-};
-
-const getCookieString = (cookies: Record<string, string>) =>
-    Object.entries(cookies)
-        .map(([k, v]) => `${k}=${v}`)
-        .join('; ');
-
-const constructHeaders = (
-    rawHeaders: Record<string, string> = {},
-    headerRules: AssignmentRule[],
-    cookieRules: AssignmentRule[]
-) => {
-    const headers = { ...rawHeaders };
-    headers['X-Origin'] = 'Aspecto-CLI';
-
-    headerRules.forEach((r) => {
-        headers[r.assignment.assignToPath] = getDynamicParam(r);
-    });
-    if (cookieRules.length === 0) return headers;
-
-    const dynamicCookies: Record<string, string> = {};
-    cookieRules.forEach((r) => {
-        dynamicCookies[r.assignment.assignToPath] = getDynamicParam(r);
-    });
-    const existingCookieHeaderKey = Object.keys(headers).find((k) => k.toLowerCase() === 'cookie');
-    if (!existingCookieHeaderKey) {
-        headers.Cookie = getCookieString(dynamicCookies);
-    } else {
-        // Merge dynamic cookies with existing, overwrite if needed
-        const existingCookieHeader = headers[existingCookieHeaderKey];
-
-        const cookies: Record<string, string> = {};
-        existingCookieHeader.split('; ').forEach((nameValuePair) => {
-            const [name, value] = nameValuePair.split(/=(.+)/);
-            cookies[name] = value;
-        });
-        Object.entries(dynamicCookies).forEach(([k, v]) => (cookies[k] = v));
-        headers[existingCookieHeaderKey] = getCookieString(cookies);
-    }
-    return headers;
 };
 
 const constructUrl = (originalRoute: string, envUrl: string, assignmentRules: AssignmentRule[]): string => {
