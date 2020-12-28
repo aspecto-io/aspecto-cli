d72e4def7f3b596cf3a59df51a914290051d129c
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2020-12-28 18:03:11 +0200

    feat: add x-cli-token auth

--- src/services/api-tests-client.ts
+++ src/services/api-tests-client.ts
@@ -4,6 +4,6 @@ const baseUrl = process.env.API_TESTS_BASE_URL ?? 'https://api-tests.aspecto.io'
 
 export const client = axios.create({ baseURL: `${baseUrl}/api/v2/test` });
 client.interceptors.request.use((config) => {
-    config.headers = { authorization: `Basic ${global.aspectoOptions.token}` };
+    config.headers = { 'X-CLI-TOKEN': global.aspectoOptions.token };
     return config;
 });
